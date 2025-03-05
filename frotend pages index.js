11170c22facc7b975a9c462364997af4c2c5494f
JaviLara-Script <Javierlara2005@gmail.com>
2025-03-05 14:13:09 -0600

    Create frotend pages index.js

---
+++ frotend pages index.js
@@ -0,0 +1,10 @@
+import HabitList from '../components/HabitList';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Gestor de Hábitos</h1>
+      <HabitList />
+    </div>
+  );
+}
